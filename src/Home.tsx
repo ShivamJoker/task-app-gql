7e17a3a55472abadc0a9d8798f925fdd5b4eff56
Shivam <singhshivamkr@gmail.com>
2022-05-20 15:42:56 +0530

    :sparkles: Basic fetching of tasks

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,19 +1,58 @@
-import React from "react";
+import { SpinnerInfinity } from "spinners-react";
 import { useTypedQuery } from "./zeus/reactQuery";
 
 const Home = () => {
-  const { data } = useTypedQuery({
-    allTasks: {
-      items: {
-        description: true,
-        id: true,
-        taskStatus: true,
-        title: true,
-      },
+  const { data, isLoading, error } = useTypedQuery(
+    "allTask",
+    {
+      allTasks: [
+        {},
+        {
+          items: {
+            description: true,
+            id: true,
+            taskStatus: true,
+            title: true,
+          },
+        },
+      ],
     },
-  });
-  console.log(data);
-  return <div></div>;
+    // do not retry and refetch on window change
+    { retry: false, refetchOnWindowFocus: false }
+  );
+
+  // console.log(data);
+  // console.log(JSON.stringify(error));
+
+  if (isLoading) {
+    return (
+      <SpinnerInfinity
+        style={{ margin: " auto" }}
+        size={280}
+        color="#36ad47"
+        secondaryColor="rgba(0, 0, 0, 0.44)"
+      />
+    );
+  }
+
+  if (error) {
+    return (
+      <pre className="error">
+        An error occured {"\n"}
+        {JSON.stringify(error, null, 2)}
+      </pre>
+    );
+  }
+  return (
+    <>
+      <h1>Your tasks</h1>
+      <ol>
+        {data?.allTasks?.items?.map((task) => (
+          <li key={task.id}>{task.title}</li>
+        ))}
+      </ol>
+    </>
+  );
 };
 
 export default Home;
